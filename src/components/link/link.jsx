8a96a3fae692eef755060b77ebdd43ad08b301d2
Andrey Shahov <andreyka.shakhov@mail.ru>
2023-01-22 14:54:29 +0300

    feature: header

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -2,9 +2,9 @@ import linkStyles from './link.module.css';
 
 export default function Link({ icon, name, color}) {
   return (
-    <li className={linkStyles.box}>
+    <div className={`${linkStyles.box} pl-5 pr-5`}>
       <div className="mr-2">{icon}</div>
       <a href="#" className={`${linkStyles.link} text text_type_main-default ${color}`}>{name}</a>
-    </li>
+    </div>
   )
 }
