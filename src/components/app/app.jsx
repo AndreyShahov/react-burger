3d2d3055c8f8e1602bb54fc99af59892ed0c4ac6
Andrey Shahov <andreyka.shakhov@mail.ru>
2023-01-28 09:51:11 +0300

    refactor: ingredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getIngredients } from '../api/api.js';
 import BurgerIngredient from '../burger-ingredient/burger-ingredient.jsx';
 import { config } from '../data/data.js';
@@ -9,6 +9,10 @@ import appStyles from './app.module.css';
 
 function App() {
   const [ingredients, setIngredients] = useState(null);
+  const bunRef = useRef(null);
+  const sauceRef = useRef(null);
+  const mainRef = useRef(null);
+
   useEffect(() => {
     getIngredients(config)
       .then(res => {
@@ -17,6 +21,9 @@ function App() {
       .catch(err => console.log(err));
   }, [])
 
+ function onButtonClick(ref) {
+  ref.current.focus();
+ }
 
 
 
@@ -27,7 +34,7 @@ function App() {
       <BurgerIngredients>
         <li>
           <h2>Булки</h2>
-          <div className={appStyles.container}>
+          <div className={appStyles.container} ref={bunRef}>
             {ingredients &&
               ingredients.data.filter(item => item.type == 'bun').map(item => {
                 return <BurgerIngredient {...item} key={item['_id']} />
@@ -37,18 +44,20 @@ function App() {
         <li>
           <h2>Соусы</h2>
           <div className={appStyles.container}>
-            {ingredients.data.filter(item => item.type == 'sauce').map(item => {
-              return <BurgerIngredient {...item} key={item['_id']} />
-            })}
+            {ingredients &&
+              ingredients.data.filter(item => item.type == 'sauce').map(item => {
+                return <BurgerIngredient {...item} key={item['_id']} />
+              })}
           </div>
         </li>
         <li>
           <h2>Начинки</h2>
-          <div className={appStyles.container}>{ingredients.data.filter(item => item.type == 'main').map(item => {
-            return <BurgerIngredient {...item} key={item['_id']} />
-          })}</div>
+          <div className={appStyles.container}>
+            {ingredients &&
+              ingredients.data.filter(item => item.type == 'main').map(item => {
+                return <BurgerIngredient {...item} key={item['_id']} />
+              })}</div>
         </li>
-
       </BurgerIngredients>
     </>
   )
