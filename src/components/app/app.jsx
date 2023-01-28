aa89ed2d5653459756d294971230a9dbac2d30fc
Andrey Shahov <andreyka.shakhov@mail.ru>
2023-01-28 10:31:57 +0300

    feature: scroll necessary category

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { getIngredients } from '../api/api.js';
 import BurgerIngredient from '../burger-ingredient/burger-ingredient.jsx';
 import { config } from '../data/data.js';
@@ -9,9 +9,6 @@ import appStyles from './app.module.css';
 
 function App() {
   const [ingredients, setIngredients] = useState(null);
-  const bunRef = useRef(null);
-  const sauceRef = useRef(null);
-  const mainRef = useRef(null);
 
   useEffect(() => {
     getIngredients(config)
@@ -31,33 +28,8 @@ function App() {
   return (
     <>
       <AppHeader />
-      <BurgerIngredients>
-        <li>
-          <h2>Булки</h2>
-          <div className={appStyles.container} ref={bunRef}>
-            {ingredients &&
-              ingredients.data.filter(item => item.type == 'bun').map(item => {
-                return <BurgerIngredient {...item} key={item['_id']} />
-              })}
-          </div>
-        </li>
-        <li>
-          <h2>Соусы</h2>
-          <div className={appStyles.container}>
-            {ingredients &&
-              ingredients.data.filter(item => item.type == 'sauce').map(item => {
-                return <BurgerIngredient {...item} key={item['_id']} />
-              })}
-          </div>
-        </li>
-        <li>
-          <h2>Начинки</h2>
-          <div className={appStyles.container}>
-            {ingredients &&
-              ingredients.data.filter(item => item.type == 'main').map(item => {
-                return <BurgerIngredient {...item} key={item['_id']} />
-              })}</div>
-        </li>
+      <BurgerIngredients ingredients={ingredients}>
+
       </BurgerIngredients>
     </>
   )
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,28 +1,65 @@
 import React from 'react';
+import { useRef } from 'react'
 import ingredientsStyles from './burger-ingredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredient from '../burger-ingredient/burger-ingredient';
 import { isPropertySignature } from 'typescript';
 
-export default function BurgerIngredients(props) {
-  const [current, setCurrent] = React.useState('one')
+export default function BurgerIngredients({ ingredients }) {
+  const [current, setCurrent] = React.useState('one');
+  const bunRef = useRef(null);
+  const sauceRef = useRef(null);
+  const mainRef = useRef(null);
+
+function handleClick(value, ref) {
+  setCurrent(value);
+  ref.current.scrollIntoView();
+ }
+
 
   return (
     <main className={ingredientsStyles.main}>
       <section className={`${ingredientsStyles.section} pt-10`}>
         <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
         <div className="mb-10" style={{ display: 'flex' }}>
-          <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+          <Tab value="one" active={current === 'one'} onClick={(value) => handleClick(value, bunRef)}>
             Булки
           </Tab>
-          <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+          <Tab value="two" active={current === 'two'} onClick={(value) => handleClick(value, sauceRef)}>
             Соусы
           </Tab>
-          <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+          <Tab value="three" active={current === 'three'} onClick={(value) => handleClick(value, mainRef)}>
             Начинки
           </Tab>
         </div>
-        <ul className={`${ingredientsStyles.menu} ml-4`}>{props.children}</ul>
+        <ul className={`${ingredientsStyles.menu} ml-4`}>
+          <li>
+            <h2  ref={bunRef}>Булки</h2>
+            <div className={ingredientsStyles.container}>
+              {ingredients &&
+                ingredients.data.filter(item => item.type == 'bun').map(item => {
+                  return <BurgerIngredient {...item} key={item['_id']} />
+                })}
+            </div>
+          </li>
+          <li>
+            <h2 ref={sauceRef}>Соусы</h2>
+            <div className={ingredientsStyles.container}>
+              {ingredients &&
+                ingredients.data.filter(item => item.type == 'sauce').map(item => {
+                  return <BurgerIngredient {...item} key={item['_id']} />
+                })}
+            </div>
+          </li>
+          <li>
+            <h2 ref={mainRef}>Начинки</h2>
+            <div className={ingredientsStyles.container}>
+              {ingredients &&
+                ingredients.data.filter(item => item.type == 'main').map(item => {
+                  return <BurgerIngredient {...item} key={item['_id']} />
+                })}</div>
+          </li>
+        </ul>
       </section>
     </main>
   )
