671f68e675a6fddb761c9c92f47cce733e4766d3
Andrey Shahov <andreyka.shakhov@mail.ru>
2023-02-24 23:08:42 +0300

    refactor: burgerIngredients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useRef, useState } from 'react'
 import ingredientsStyles from './burger-ingredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import BurgerIngredient from '../burger-ingredient/burger-ingredient';
 import { Modal } from '../modal/modal';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 import PropTypes from 'prop-types';
+import { IngredientCategory } from '../ingredient-category/ingredient-category';
 
 export default function BurgerIngredients({ ingredients }) {
   const [current, setCurrent] = React.useState('one');
@@ -17,9 +17,20 @@ export default function BurgerIngredients({ ingredients }) {
 
   const [ingredientInModal, setIngredientInModal] = useState(null);
 
-  const bunArray = ingredients.data.filter(item => item.type == 'bun');
-  const sauceArray = ingredients.data.filter(item => item.type == 'sauce');
-  const mainArray = ingredients.data.filter(item => item.type == 'main');
+  const bunArray = React.useMemo(
+    () => ingredients.data.filter(item => item.type == 'bun'),
+    [ingredients]
+  );
+
+  const sauceArray = React.useMemo(
+    () => ingredients.data.filter(item => item.type == 'sauce'),
+    [ingredients]
+  );
+
+  const mainArray = React.useMemo(
+    () => ingredients.data.filter(item => item.type == 'main'),
+    [ingredients]
+  );
 
   function handleClick(value, ref) {
     setCurrent(value);
@@ -41,54 +52,27 @@ export default function BurgerIngredients({ ingredients }) {
         </Tab>
       </div>
       <ul className={`${ingredientsStyles.menu} ml-4`}>
-        <li>
-          <h2 className="text text_type_main-medium mt-10 mb-6" ref={bunRef}>Булки</h2>
-          <div className={ingredientsStyles.container}>
-            {bunArray.map(item => {
-              return (
-                <BurgerIngredient
-                  ingredientData={item}
-                  key={item['_id']}
-                  count={1}
-                  onClick={setIngredientInModal}
-                  setIsModal={setIsModal}
-                />
-              )
-            })}
-          </div>
-        </li>
-        <li>
-          <h2 className="text text_type_main-medium mt-10 mb-6" ref={sauceRef}>Соусы</h2>
-          <div className={ingredientsStyles.container}>
-            {sauceArray.map(item => {
-              return (
-                <BurgerIngredient
-                  ingredientData={item}
-                  key={item['_id']}
-                  count={1}
-                  onClick={setIngredientInModal}
-                  setIsModal={setIsModal}
-                />
-              )
-            })}
-          </div>
-        </li>
-        <li>
-          <h2 className="text text_type_main-medium mt-10 mb-6" ref={mainRef}>Начинки</h2>
-          <div className={ingredientsStyles.container}>
-            {mainArray.map(item => {
-              return (
-                <BurgerIngredient
-                  ingredientData={item}
-                  key={item['_id']}
-                  count={1}
-                  onClick={setIngredientInModal}
-                  setIsModal={setIsModal}
-                />
-              )
-            })}
-          </div>
-        </li>
+        <IngredientCategory
+          ingredientRef={bunRef}
+          header={'Булки'}
+          bunArray={bunArray}
+          setIsModal={setIsModal}
+          onIngredientClick={setIngredientInModal}
+        />
+        <IngredientCategory
+          ingredientRef={sauceRef}
+          header={'Соусы'}
+          bunArray={sauceArray}
+          setIsModal={setIsModal}
+          onIngredientClick={setIngredientInModal}
+        />
+        <IngredientCategory
+          ingredientRef={mainRef}
+          header={'Начинки'}
+          bunArray={mainArray}
+          setIsModal={setIsModal}
+          onIngredientClick={setIngredientInModal}
+        />
       </ul>
       {isModal &&
         <Modal header="Детали ингредиента" setIsModal={setIsModal}>
